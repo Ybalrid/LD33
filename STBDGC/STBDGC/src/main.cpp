36c781305093bd903168d58ac07cd045532de77d
Ybalrid <ybalrid@cecmu.org>
2015-08-22 05:36:49 +0200

    Add visual studio

---
+++ STBDGC/STBDGC/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#ifdef _WIN32
+#define mainFunc() int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#else
+#define mainFunc() int main(void) 
+#endif
+
+mainFunc()
+{
+	return EXIT_SUCCESS;
+}
